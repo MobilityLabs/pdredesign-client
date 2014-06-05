dbd8f47ab51c0c3b6dfc3d021a656c46695d1cb0
Sumeet Singh <ortuna@gmail.com>
2014-06-05 15:10:31 -0400

    Added report route

--- app/assets/javascripts/client/PDRClient.js
+++ app/assets/javascripts/client/PDRClient.js
@@ -64,106 +64,119 @@ PDRClient.config(['$stateProvider', '$urlRouterProvider',
         }
       }
    })
-    .state('assessment_dashboard', {
-      url: '/assessments/:id/dashboard',
-      views: {
-        '': {
-          controller: 'AssessmentDashboardCtrl',
-          templateUrl: 'client/views/assessments/dashboard.html'
-        },
-        'sidebar': {
-          controller: 'SidebarCtrl',
-          templateUrl: 'client/views/sidebar/assessment_dashboard.html'
-        }
-      }
-    })
-    .state('assessment_assign', {
-      url: '/assessments/:id/assign',
-      views: {
-        '': {
-          controller: 'AssessmentAssignCtrl',
-          templateUrl: 'client/views/assessments/assign/assign.html'
-        }
-      }
-    })
-    .state('assessment_edit', {
-      url: '/assessments/:id/edit',
-      views: {
-        '': {
-          controller: 'AssessmentEditCtrl',
-          templateUrl: 'client/views/assessments/edit.html'
-        },
-        'sidebar': {
-          controller: 'SidebarCtrl',
-          templateUrl: 'client/views/sidebar/sidebar_generic.html'
-        }
-      }
-    })
-    .state('response_edit', {
-      url: '/assessments/:assessment_id/responses/:response_id',
-      views: {
-        '': {
-          controller: 'ResponseCtrl',
-          templateUrl: 'client/views/responses/edit.html'
-        },
-        'sidebar': {
-          controller: 'SidebarResponseCardCtrl',
-          templateUrl: 'client/views/sidebar/response_card.html'
-        }
-      }
+   .state('assessment_dashboard', {
+     url: '/assessments/:id/dashboard',
+     views: {
+       '': {
+         controller: 'AssessmentDashboardCtrl',
+         templateUrl: 'client/views/assessments/dashboard.html'
+       },
+       'sidebar': {
+         controller: 'SidebarCtrl',
+         templateUrl: 'client/views/sidebar/assessment_dashboard.html'
+       }
+     }
+   })
+   .state('assessment_dashboard', {
+     url: '/assessments/:id/report',
+     views: {
+       '': {
+         controller: 'AssessmentReportCtrl',
+         templateUrl: 'client/views/assessments/report.html'
+       },
+       'sidebar': {
+         controller: 'SidebarCtrl',
+         templateUrl: 'client/views/sidebar/assessment_dashboard.html'
+       }
+     }
+   })
+   .state('assessment_assign', {
+     url: '/assessments/:id/assign',
+     views: {
+       '': {
+         controller: 'AssessmentAssignCtrl',
+         templateUrl: 'client/views/assessments/assign/assign.html'
+       }
+     }
+   })
+   .state('assessment_edit', {
+     url: '/assessments/:id/edit',
+     views: {
+       '': {
+         controller: 'AssessmentEditCtrl',
+         templateUrl: 'client/views/assessments/edit.html'
+       },
+       'sidebar': {
+         controller: 'SidebarCtrl',
+         templateUrl: 'client/views/sidebar/sidebar_generic.html'
+       }
+     }
+   })
+   .state('response_edit', {
+     url: '/assessments/:assessment_id/responses/:response_id',
+     views: {
+       '': {
+         controller: 'ResponseCtrl',
+         templateUrl: 'client/views/responses/edit.html'
+       },
+       'sidebar': {
+         controller: 'SidebarResponseCardCtrl',
+         templateUrl: 'client/views/sidebar/response_card.html'
+       }
+     }
    })
    .state('consensus_show', {
-      url: '/assessments/:assessment_id/consensus/:response_id',
-      views: {
-        '': {
-          controller: 'ConsensusShowCtrl',
-          templateUrl: 'client/views/consensus/show.html'
-        },
-        'sidebar': {
-          controller: 'SidebarResponseCardCtrl',
-          templateUrl: 'client/views/sidebar/response_card.html'
-        }
-      }
+     url: '/assessments/:assessment_id/consensus/:response_id',
+     views: {
+       '': {
+         controller: 'ConsensusShowCtrl',
+         templateUrl: 'client/views/consensus/show.html'
+       },
+       'sidebar': {
+         controller: 'SidebarResponseCardCtrl',
+         templateUrl: 'client/views/sidebar/response_card.html'
+       }
+     }
    })
    .state('consensus_create', {
-      url: '/assessments/:assessment_id/consensus',
-      views: {
-        '': {
-          controller: 'ConsensusCreateCtrl',
-          templateUrl: 'client/views/consensus/show.html'
-        },
-        'sidebar': {
-          controller: 'SidebarResponseCardCtrl',
-          templateUrl: 'client/views/sidebar/sidebar_generic.html'
-        }
-      }
+     url: '/assessments/:assessment_id/consensus',
+     views: {
+       '': {
+         controller: 'ConsensusCreateCtrl',
+         templateUrl: 'client/views/consensus/show.html'
+       },
+       'sidebar': {
+         controller: 'SidebarResponseCardCtrl',
+         templateUrl: 'client/views/sidebar/sidebar_generic.html'
+       }
+     }
    })
    .state('signup', {
-      url: '/signup',
-      views: {
-        '': {
-          controller: 'SignupCtrl',
-          templateUrl: 'client/views/signup/member.html'
-        },
-        'sidebar': {
-          controller: 'SidebarCtrl',
-          templateUrl: 'client/views/sidebar/sidebar_login.html'
-        }
-      }
-    })
+     url: '/signup',
+     views: {
+       '': {
+         controller: 'SignupCtrl',
+         templateUrl: 'client/views/signup/member.html'
+       },
+       'sidebar': {
+         controller: 'SidebarCtrl',
+         templateUrl: 'client/views/sidebar/sidebar_login.html'
+       }
+     }
+   })
    .state('signup_facilitator', {
-      url: '/signup/facilitator',
-      views: {
-        '': {
-          controller: 'SignupCtrl',
-          templateUrl: 'client/views/signup/facilitator.html'
-        },
-        'sidebar': {
-          controller: 'SidebarCtrl',
-          templateUrl: 'client/views/sidebar/sidebar_login.html'
-        }
-      }
-    })
+     url: '/signup/facilitator',
+     views: {
+       '': {
+         controller: 'SignupCtrl',
+         templateUrl: 'client/views/signup/facilitator.html'
+       },
+       'sidebar': {
+         controller: 'SidebarCtrl',
+         templateUrl: 'client/views/sidebar/sidebar_login.html'
+       }
+     }
+   })
 
     .state('settings', {
       url: '/settings',
