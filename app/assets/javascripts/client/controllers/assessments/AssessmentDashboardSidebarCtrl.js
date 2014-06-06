5ee1505580ff909a12419563bdcd74999075b974
Michael Davis <mkizziah38@gmail.com>
2014-06-06 14:31:28 -0400

    Fixed Assessment dashboard sidebar links.

--- app/assets/javascripts/client/controllers/assessments/AssessmentDashboardSidebarCtrl.js
+++ app/assets/javascripts/client/controllers/assessments/AssessmentDashboardSidebarCtrl.js
@@ -10,6 +10,24 @@ PDRClient.controller('AssessmentDashboardSidebarCtrl', ['$scope', '$timeout', 'S
         }
       }
 
+      $scope.showConsensusCreateLink = function() {
+        if(typeof $scope.assessment.overview !== "undefined") {
+          return $scope.assessment.overview.link == "edit_consensus";
+        }
+      }
+
+      $scope.showConsensusViewLink = function() {
+        if(typeof $scope.assessment.overview !== "undefined") {
+          return $scope.assessment.overview.link == "view_consensus";
+        }
+      }
+
+
+      $scope.showModifyScheduleLink = function() {
+        return moment().isBefore($scope.assessment.meeting_date)
+      }
+
+
       $scope.meetingDayName = function(date) {
         if (date !== null) {
           return moment(date).format("dddd");
