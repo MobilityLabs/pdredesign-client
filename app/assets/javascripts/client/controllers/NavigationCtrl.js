556815c6f4f619e94fbf6500762635b4eccf389f
Sumeet Singh <ortuna@gmail.com>
2014-06-09 16:03:30 -0400

    Merged Navigation hightlight state
    closes #19

--- app/assets/javascripts/client/controllers/NavigationCtrl.js
+++ app/assets/javascripts/client/controllers/NavigationCtrl.js
@@ -1,17 +1,29 @@
-PDRClient.controller('NavigationCtrl', ['$scope', '$rootScope', 'SessionService',
-    function($rootScope, $scope, SessionService) {
+PDRClient.controller('NavigationCtrl', ['$scope', '$rootScope', 'SessionService', '$location',
+    function($rootScope, $scope, SessionService, $location) {
 
       $scope.user = SessionService.getCurrentUser();
 
       $scope.updateTemplate = function() {
         SessionService.setUserTemplate(
-          $scope, 
-          'client/views/navigation/navigation_user.html', 
+          $scope,
+          'client/views/navigation/navigation_user.html',
           'client/views/navigation/navigation_anon.html'
         );
       };
-
+      $scope.locationHolder = '';
       $scope.updateTemplate();
+      $scope.user = SessionService.getCurrentUser();
+
+      $scope.$watch(function () { return $location.url(); }, function (url) {
+        switch(url) {
+          case '/assessments':
+            return $scope.locationHolder = "currentStates";
+          case '':
+            return $scope.locationHolder = "home";
+          default:
+            return '';
+        }
+      })
 
       $scope.$on('session_updated', function() {
         $scope.updateTemplate();
