bb1aeaf887c4cef35fd2185a80d90b0828ad77b9
Michael Davis <mkizziah38@gmail.com>
2014-06-10 17:48:16 -0400

    Assessment Dashboard link active added.

--- app/assets/javascripts/client/PDRClient.js
+++ app/assets/javascripts/client/PDRClient.js
@@ -194,3 +194,8 @@ PDRClient.config(['$stateProvider', '$urlRouterProvider',
 
   }
 ]);
+
+angular.module("PDRClient").run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
