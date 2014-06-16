ade1d3e1f97faa07aa0ea26ed67f55dff92a1e25
Sumeet Singh <ortuna@gmail.com>
2014-06-16 16:02:32 -0400

    Variance + category sort

--- app/assets/javascripts/client/directives/consensus/consensus.js
+++ app/assets/javascripts/client/directives/consensus/consensus.js
@@ -72,7 +72,44 @@ PDRClient.directive('consensus', [
 
           $scope.viewModes = [{label: "Category"}, {label: "Variance"}];
           $scope.viewMode  = $scope.viewModes[0];
-          $scope.$on('submit_response', function(){
+          
+          $scope.sortByCategory = function() {
+            return $scope.data;
+          };
+
+          $scope.sortByVariance = function(categories) {
+            var tmpObject = {};
+            var keys      = [];
+
+            angular.forEach(categories, function(category, _key) {
+              angular.forEach(category.questions, function(question, key) {
+                if(typeof tmpObject[question.variance] == 'undefined')
+                  tmpObject[question.variance] = {"name": question.variance, "questions": []};
+                keys.push(question.variance);
+                tmpObject[question.variance]["questions"].push(question);
+              });
+            });
+
+            keys.sort();
+
+            var sorted    = {};
+            angular.forEach(keys, function(key) { sorted[key] = tmpObject[key]; }); 
+
+            return sorted;
+          };
+
+          $scope.changeViewMode = function(mode) {
+            switch(mode.toLowerCase()) {
+              case 'variance':
+                $scope.categories = $scope.sortByVariance($scope.data);
+                break;
+              case 'category':
+                $scope.categories = $scope.sortByCategory();
+                break;
+            }
+          };
+          
+          $scope.$on('submit_response', function() {
             Consensus
               .submit({assessment_id: $scope.assessmentId, id: $scope.responseId}, {submit: true})
               .$promise
@@ -81,13 +118,13 @@ PDRClient.directive('consensus', [
               });
           });
 
-
           $timeout(function(){
             Consensus
               .get({assessment_id: $scope.assessmentId, id: $scope.responseId})
               .$promise
-              .then(function(data){
+              .then(function(data) {
                 $scope.scores     = data.scores;
+                $scope.data       = data.categories;
                 $scope.categories = data.categories;
                 $scope.isReadOnly = data.is_completed || false;
                 $scope.participantCount = data.participant_count;
