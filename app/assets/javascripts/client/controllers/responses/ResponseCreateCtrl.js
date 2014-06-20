87626c2956858092b799cd1f2cc6e05bc5ac5f41
Michael Davis <mkizziah38@gmail.com>
2014-06-20 10:25:40 -0400

    ResponseCreate with Spec.

---
+++ app/assets/javascripts/client/controllers/responses/ResponseCreateCtrl.js
@@ -0,0 +1,31 @@
+PDRClient.controller('ResponseCreateCtrl', ['$timeout', '$scope', '$location', 'SessionService', 'Response', '$stateParams', 'Assessment',
+  function($timeout, $scope, $location, SessionService, Response, $stateParams, Assessment) {
+    $scope.errors  = null;
+
+    $scope.assessmentId = $stateParams.assessment_id;
+
+    $scope.getAssessment = function(){
+      return Assessment.get({id: $scope.assessmentId});
+    };
+
+    $scope.createResponse = function(assessmentId, rubric_id) {
+      Response
+      .save({assessment_id: assessmentId},  {rubric_id: rubric_id})
+      .$promise
+      .then(function(response){
+        $location.path('/assessments/'+ $scope.assessmentId +'/responses/' + response.id);
+      }, function(data){
+        console.log("failure");
+        $scope.errors  = "Response was not created.";
+      });
+    };
+
+    $timeout(function(){
+      $scope.getAssessment()
+        .$promise
+        .then(function(assessment){
+          $scope.createResponse($scope.assessmentId, assessment.rubric_id)
+        });
+    });
+  }
+]);
