dc284abcc7cef3765ae43e257b0b60eda50f2ef4
Michael Davis <mkizziah38@gmail.com>
2014-07-07 16:07:23 -0400

    Error Alert added to redeem.html. InvitationCtrlSpec created.

--- app/assets/javascripts/client/controllers/invitation/InvitationCtrl.js
+++ app/assets/javascripts/client/controllers/invitation/InvitationCtrl.js
@@ -1,19 +1,38 @@
-PDRClient.controller('InvitationCtrl', ['$scope', '$location', '$stateParams', 'Invitation', 'SessionService', 
+PDRClient.controller('InvitationCtrl', ['$scope', '$location', '$stateParams', 'Invitation', 'SessionService',
     function($scope, $location, $stateParams, Invitation, SessionService) {
       $scope.token        = $stateParams.token;
       $scope.invitedUser  = Invitation.get({token: $scope.token});
+      $scope.inviteObject = {}
+      $scope.isError = null;
+      $scope.errors   = null;
+      $scope.showalert = false;
+      $scope.alerts = [];
+
+      $scope.showError = function() {
+        $scope.alerts.push({type: 'danger', msg: 'Invalid email or password!'});
+      };
+
+      $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+      };
+
+
       $scope.redeemInvite = function() {
-        var inviteObject = {
+        $scope.inviteObject = {
           first_name: $scope.invitedUser.first_name,
           last_name:  $scope.invitedUser.last_name,
           password:   $scope.invitedUser.password,
           email:      $scope.invitedUser.email,
         };
         Invitation
-          .save({token: $scope.token}, inviteObject)
+          .save({token: $scope.token}, $scope.inviteObject)
           .$promise
           .then(function() {
             $location.url('/login');
+          }, function(data){
+              console.log(data)
+              $scope.showError();
+
           });
       };
     }
