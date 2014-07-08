6fd57dd2c4513788a9a418b8c28f09ad8d63644b
Michael Davis <mkizziah38@gmail.com>
2014-07-08 12:17:36 -0400

    Learn more about PD Redesign modal on sidebar refactored.

--- app/assets/javascripts/client/controllers/sidebars/SidebarCtrl.js
+++ app/assets/javascripts/client/controllers/sidebars/SidebarCtrl.js
@@ -1,5 +1,5 @@
-PDRClient.controller('SidebarCtrl', ['$scope', 'SessionService',
-    function($scope, SessionService) {
+PDRClient.controller('SidebarCtrl', ['$scope', '$modal', 'SessionService',
+    function($scope, $modal, SessionService) {
       $scope.user   = SessionService.getCurrentUser();
       if($scope.user) {
         $scope.visible = true;
@@ -7,5 +7,18 @@ PDRClient.controller('SidebarCtrl', ['$scope', 'SessionService',
         $scope.avatar = $scope.user.avatar;
         $scope.role   = $scope.user.role;
       }
+
+      $scope.pdrOverview  = function() {
+        $scope.modal = $modal.open({
+          templateUrl: 'client/views/modals/pdr_overview.html',
+          scope: $scope
+        });
+      };
+
+      $scope.close = function() {
+        $scope.modal.dismiss('cancel');
+      }
+
     }
+
 ]);
