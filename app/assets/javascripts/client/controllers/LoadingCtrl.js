3f20f793cb1c0b51208acea962a4f96744d65a52
Sumeet Singh <ortuna@gmail.com>
2014-07-10 19:49:37 -0400

    100% height loading screen

--- app/assets/javascripts/client/controllers/LoadingCtrl.js
+++ app/assets/javascripts/client/controllers/LoadingCtrl.js
@@ -1,10 +1,10 @@
 PDRClient.controller('LoadingCtrl', ['$scope', 'SessionService',
     function($scope, SessionService) {
-      $scope.$on('start_change', function(){
+      $scope.$on('start_change', function() {
         $scope.loading = true;
       });
 
-      $scope.$on('success_change', function(){
+      $scope.$on('success_change', function() {
         $scope.loading = false;
       });
     }
