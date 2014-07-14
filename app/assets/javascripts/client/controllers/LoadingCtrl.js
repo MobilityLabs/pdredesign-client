4237ebffa2a601df867040df2dbb7d7d00725a17
Sumeet Singh <ortuna@gmail.com>
2014-07-14 10:53:50 -0400

    Fixing loading height on loader

--- app/assets/javascripts/client/controllers/LoadingCtrl.js
+++ app/assets/javascripts/client/controllers/LoadingCtrl.js
@@ -1,6 +1,9 @@
 PDRClient.controller('LoadingCtrl', ['$scope', 'SessionService',
     function($scope, SessionService) {
       $scope.$on('start_change', function() {
+        $('.loading-state')
+          .height($(document).height());
+
         $scope.loading = true;
       });
 
