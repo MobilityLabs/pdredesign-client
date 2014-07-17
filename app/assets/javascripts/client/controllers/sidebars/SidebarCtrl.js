acad9a1a521bd0ff31b79b97d66c931ba7bcd54f
Sumeet Singh <ortuna@gmail.com>
2014-07-17 15:18:04 -0400

    Dont check avatar

--- app/assets/javascripts/client/controllers/sidebars/SidebarCtrl.js
+++ app/assets/javascripts/client/controllers/sidebars/SidebarCtrl.js
@@ -3,7 +3,7 @@ PDRClient.controller('SidebarCtrl', ['$scope', '$modal', '$stateParams', 'Sessio
       $scope.user     = SessionService.getCurrentUser();
       $scope.redirect = $stateParams.redirect;
 
-      if($scope.user && $scope.user["avatar"]) {
+      if($scope.user) {
         $scope.visible = true;
         $scope.name   = $scope.user["full_name"];
         $scope.avatar = $scope.user["avatar"];
