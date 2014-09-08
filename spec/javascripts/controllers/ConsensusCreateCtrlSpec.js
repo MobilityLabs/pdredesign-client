8a07c7db3f46d1128709e8fe518e7c53dcde579e
Sumeet Singh <ortuna@gmail.com>
2014-09-08 13:56:34 -0400

    Tab fix

--- spec/javascripts/controllers/ConsensusCreateCtrlSpec.js
+++ spec/javascripts/controllers/ConsensusCreateCtrlSpec.js
@@ -8,16 +8,16 @@ describe('Controller: ConsensusCreateCtrl', function() {
   beforeEach(module('PDRClient'));
 
   beforeEach(inject(function($injector, $controller) {
-      q  = $injector.get('$q');
-      scope              = $injector.get('$rootScope').$new();
-      httpBackend        = $injector.get('$httpBackend');
-      ConsensusResource  = $injector.get('Consensus');
-      timeout            = $injector.get('$timeout');
+    q  = $injector.get('$q');
+    scope              = $injector.get('$rootScope').$new();
+    httpBackend        = $injector.get('$httpBackend');
+    ConsensusResource  = $injector.get('Consensus');
+    timeout            = $injector.get('$timeout');
 
-      subject  = $controller('ConsensusCreateCtrl', {
-        $scope: scope
-      });
-      scope.assessmentId = 2
+    subject  = $controller('ConsensusCreateCtrl', {
+      $scope: scope
+    });
+    scope.assessmentId = 2
 
   }));
 
@@ -28,43 +28,43 @@ describe('Controller: ConsensusCreateCtrl', function() {
 
   it('Consensus create is called', function() {
     spyOn(ConsensusResource, 'create')
-      .and.callFake(function() {
-          var deferred = q.defer();
-          deferred.resolve({});
-          return {$promise: deferred.promise};
+    .and.callFake(function() {
+      var deferred = q.defer();
+      deferred.resolve({});
+      return {$promise: deferred.promise};
     });
-      timeout.flush();
-      expect(ConsensusResource.create).toHaveBeenCalled();
+    timeout.flush();
+    expect(ConsensusResource.create).toHaveBeenCalled();
   });
 
   it('Consensus create is called', function() {
-        httpBackend.expectPOST('/v1/assessments/2/consensus').respond({})
-        scope.createConsensus(2, 3);
-        httpBackend.flush();
+    httpBackend.expectPOST('/v1/assessments/2/consensus').respond({})
+    scope.createConsensus(2, 3);
+    httpBackend.flush();
   });
 
   it('Consensus create fails and isError is true', function() {
-      spyOn(ConsensusResource, 'create').and.callFake(function(){
-        var deferred = q.defer();
-        deferred.reject({});
-        return {$promise: deferred.promise};
-      });
+    spyOn(ConsensusResource, 'create').and.callFake(function(){
+      var deferred = q.defer();
+      deferred.reject({});
+      return {$promise: deferred.promise};
+    });
 
-      scope.createConsensus()
-      scope.$apply();
-      expect(scope.isError).toEqual(true);
+    scope.createConsensus()
+    scope.$apply();
+    expect(scope.isError).toEqual(true);
   });
 
   it('Consensus create is successful and has no errors', function() {
-      spyOn(ConsensusResource, 'create').and.callFake(function(){
-        var deferred = q.defer();
-        deferred.resolve({});
-        return {$promise: deferred.promise};
-      });
+    spyOn(ConsensusResource, 'create').and.callFake(function(){
+      var deferred = q.defer();
+      deferred.resolve({});
+      return {$promise: deferred.promise};
+    });
 
-      scope.createConsensus()
-      scope.$apply();
-      expect(scope.isError).toEqual(null);
+    scope.createConsensus()
+    scope.$apply();
+    expect(scope.isError).toEqual(null);
   });
 
 
