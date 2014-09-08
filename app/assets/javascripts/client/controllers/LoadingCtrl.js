9a48f9440b862b5029d81c0dcbc77fe67cc04297
Sumeet Singh <ortuna@gmail.com>
2014-09-08 15:28:47 -0400

    closes #131
    Squashed commit of the following:
    
    commit 4f1caa32c894b30b7d8c6fc0db535c8915e72a2e
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Thu Sep 4 16:01:14 2014 -0400
    
        Removed unused file.
    
    commit 6a72c53fe14fdf40cd5943066c2d491685d40c0e
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Thu Sep 4 15:35:59 2014 -0400
    
        Popovers are hidden during resolve loading state.
    
    commit b055bde1935e49177b392631629d0e2661584fa7
    Merge: c7b3190 5b264e8
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Wed Sep 3 13:31:51 2014 -0400
    
        Merge branch 'master' of github.com:MobilityLabs/pdr-client
    
    commit c7b31903ba09ee3bde8e1b95e4363139c1e5b505
    Merge: 5a67767 827afdb
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Wed Sep 3 13:13:09 2014 -0400
    
        Merge branch 'master' of github.com:MobilityLabs/pdr-client
    
        Conflicts:
        	app/assets/javascripts/client/controllers/assessments/AssessmentsCtrl.js
        	app/assets/javascripts/client/views/assessments/assessment-list.html
        	app/assets/javascripts/client/views/assessments/district-filter.html
        	app/assets/javascripts/client/views/assessments/index.html
    
    commit 5a67767608b4167af6176764d4024c41f0e5a8f4
    Merge: bfcb30d f48ae94
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Fri Aug 29 10:52:35 2014 -0400
    
        Merge branch 'master' of github.com:MobilityLabs/pdr-client
    
    commit bfcb30d221a07474dceb2bf637d747b9fc727228
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Thu Aug 21 13:41:43 2014 -0400
    
        Merge.
    
    commit 6050bc19c968bb765eb0ae38c376026481b0c6d4
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Thu Aug 21 12:13:29 2014 -0400
    
        Filters working.

--- app/assets/javascripts/client/controllers/LoadingCtrl.js
+++ app/assets/javascripts/client/controllers/LoadingCtrl.js
@@ -1,9 +1,9 @@
 PDRClient.controller('LoadingCtrl', ['$scope', 'SessionService',
     function($scope, SessionService) {
       $scope.$on('start_change', function() {
+        $('.popover').popover('hide');
         $('.loading-state')
           .height($(document).height());
-
         $scope.loading = true;
       });
 
