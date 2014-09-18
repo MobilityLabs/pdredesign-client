d8e318c6c594c5b9ccfdf50a3d9baed6215f909e
Sumeet Singh <ortuna@gmail.com>
2014-09-18 10:22:59 -0400

    Added specs

---
+++ spec/javascripts/directives/walkThroughSpec.js
@@ -0,0 +1,40 @@
+describe('Directive: walkThrough', function() {
+  var $scope,
+      $compile,
+      $httpBackend,
+      $modal,
+      element,
+      isolatedScope;
+
+  beforeEach(module('PDRClient'));
+  beforeEach(inject(function($rootScope, $injector) {
+    $scope       = $rootScope.$new();
+    $compile     = $injector.get('$compile');
+    $httpBackend = $injector.get('$httpBackend');
+    $modal       = $injector.get('$modal');
+
+    element = angular.element('<walk-through id="42"></walk-through>');
+    $compile(element)($scope);
+    $scope.$digest();
+
+    isolatedScope = element.isolateScope();
+  }));
+
+  it('assigns the correct walk through id', function() {
+    expect(isolatedScope.id).toEqual('42');
+  });
+
+  it('queries the api for an update', function() {
+    $httpBackend.expectGET('/v1/walk_throughs/42').respond({});
+    isolatedScope.updateWalkThrough(42);
+    $httpBackend.flush();
+  });
+
+  it('opens a new modal', function() {
+    spyOn($modal, 'open').and.callThrough();
+    isolatedScope.showModal();
+
+    expect($modal.open).toHaveBeenCalled();
+  });
+
+});
