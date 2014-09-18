12ae8a386060eca6ca2ab761c8b59b2d0b9322ae
Sumeet Singh <ortuna@gmail.com>
2014-09-18 08:47:45 -0400

    Hide [?] on questions when a key question/sample
    evidence is missing.
    
    - Specs updated to be cleaner
    
    closes #172
    
    Squashed commit of the following:
    
    commit 2ec8c1cc89b16f671eb3e33413d54259a417d189
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Wed Sep 17 17:01:37 2014 -0400
    
        Refactor spec file.
    
    commit 4a31d54247ea8cdb4bbb47364b778cca692359cc
    Author: Michael Davis <mkizziah38@gmail.com>
    Date:   Wed Sep 17 13:32:27 2014 -0400
    
        Questionmarks without sample evidence are hidden.

--- spec/javascripts/directives/responsequestionSpec.js
+++ spec/javascripts/directives/responsequestionSpec.js
@@ -1,45 +1,69 @@
 describe('Directive: responsequestion', function() {
-  var scope, element, compile, timeout;
+  var element,
+    isolatedScope,
+    $scope,
+    $compile,
+    $timeout,
+    $q,
+    $httpBackend;
+
   var score1 = {id: 1, evidence: "hello", value: 1, editMode: null};
   var question1 = {id: 1, score: score1 };
   var answer1 = {id: 1, value: 2};
 
   beforeEach(module('PDRClient'));
 
-  beforeEach(inject(function($rootScope, $compile, $timeout, $q) {
-    scope   = $rootScope.$new();
+  beforeEach(inject(function($rootScope, $injector) {
+
+    $scope   = $rootScope.$new();
+    $compile = $injector.get('$compile');
+    $httpBackend = $injector.get('$httpBackend');
+    $timeout = $injector.get('$timeout');
+    $q = $injector.get('$q');
+
+    $httpBackend.when('GET', '/v1/assessments/1/participants/all').respond([{id: 1}]);
     element = angular.element('<responsequestion data-assessment-id=1 data-response-id=1></responsequestion>');
-    timeout = $timeout;
-    q = $q;
-    $compile(element)(scope);
-    scope.$digest();
-    scope = scope.$$childTail
+    $compile(element)($scope);
+    $scope.$digest();
+    isolateScope = element.isolateScope();
   }));
 
   it('save button is not disabled with evidence present', function() {
-    expect(scope.invalidEvidence(question1)).toEqual(false)
+    expect(isolateScope.invalidEvidence(question1)).toEqual(false)
   });
 
   it('save button is disabled with no evidence', function() {
     var score2 = {id: 1, evidence: "", value: 1, editMode: null};
     var question2 = {id: 1, score: score2 };
-    expect(scope.invalidEvidence(question2)).toEqual(true)
+    expect(isolateScope.invalidEvidence(question2)).toEqual(true)
+  });
+
+  it('hides key_question popover when it is not included', function() {
+    var categories =  [{id: 1, name: "Category",
+                        questions: [{ number: 2, key_question: null,
+                          score: { evidence: null }
+                        }]
+                      }]
+
+    isolateScope.categories = categories;
+    isolateScope.$digest();
+    expect(element.find('sample-evidence.ng-isolate-scope').hasClass('ng-hide')).toEqual(true);
+
   });
 
   describe('#ResponseGET', function() {
-    var $httpBackend, subject;
-    var categories = [1, 2, 3]
+    var subject;
+    var categories = [1, 2, 3];
 
     beforeEach(inject(function($injector, Response) {
       subject = Response;
-      $httpBackend = $injector.get('$httpBackend');
       spyOn(subject, 'get')
         .and.callFake(function(params) {
-          var deferred = q.defer();
+          var deferred = $q.defer();
           deferred.resolve({categories: categories});
           return {$promise: deferred.promise};
       });
-        timeout.flush();
+        $timeout.flush();
     }));
 
     it('correctly calls function', function() {
@@ -47,7 +71,7 @@ describe('Directive: responsequestion', function() {
     });
 
     it('gets data on callback and sets categories', function() {
-      expect(scope.categories).toEqual(categories);
+      expect(isolateScope.categories).toEqual(categories);
     });
 
   });
