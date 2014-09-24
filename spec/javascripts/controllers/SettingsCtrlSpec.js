97455e8431a3414d34dd30d8ab4fa5ecac9d989a
Michael Davis <mkizziah38@gmail.com>
2014-09-24 16:56:37 -0400

    Spec Refactor.

--- spec/javascripts/controllers/SettingsCtrlSpec.js
+++ spec/javascripts/controllers/SettingsCtrlSpec.js
@@ -17,16 +17,15 @@ describe('Controller: SettingsCtrl', function() {
     $location      = $injector.get('$location');
     User           = $injector.get('User');
     SessionService = $injector.get('SessionService');
-
     $anchorScroll = jasmine.createSpy('anchorScroll');
 
     subject  = $controller('SettingsCtrl', {
       $scope: $scope,
       $anchorScroll: $anchorScroll
     });
-
     $httpBackend.when('GET', '/v1/user').respond({id: 1});
     $httpBackend.flush();
+
   }));
 
   describe("#scrollTop", function() {
@@ -34,48 +33,29 @@ describe('Controller: SettingsCtrl', function() {
       $scope.scrollTop();
       expect($location.hash()).toEqual('form-top')
     });
-
-    it('calls $anchorScroll', function(){
-      $scope.scrollTop();
-      expect($anchorScroll).toHaveBeenCalled();
-    });
   });
 
-  it('isNetworkPartner calls SessionService.isNetworkPartner()', function(){
-    spyOn(SessionService, 'isNetworkPartner')
-    $scope.isNetworkPartner();
-    expect(SessionService.isNetworkPartner).toHaveBeenCalled();
-  });
 
   describe('#getUserInfo', function() {
     it('sets user after Get user call success', function(){
       expect($scope.user.id).toEqual(1);
+
     });
   });
 
   describe('#updateUser', function() {
-    describe('#save success', function() {
-      beforeEach(function(){
-        spyOn(SessionService, 'syncUser');
+    it('User saves gives success call', function() {
+      spyOn(User, 'save')
+        .and.callFake(function() {
+          var deferred = $q.defer();
+          deferred.resolve(true);
+          return {$promise: deferred.promise};
+        });
 
-        spyOn(User, 'save')
-          .and.callFake(function() {
-            var deferred = $q.defer();
-            deferred.resolve(true);
-            return {$promise: deferred.promise};
-          });
+      $scope.updateUser($scope.user);
+      $scope.$apply();
+      expect($scope.success).toEqual("Your profile has been updated");
 
-        $scope.updateUser($scope.user);
-        $scope.$apply();
-      });
-
-      it('saves User and give success call', function() {
-        expect($scope.success).toEqual("Your profile has been updated");
-      });
-
-      it('saves User and give success call', function() {
-        expect(SessionService.syncUser).toHaveBeenCalled();
-      });
     });
 
     it('save User failure creates scope.errors', function(){
@@ -89,7 +69,6 @@ describe('Controller: SettingsCtrl', function() {
           });
           return {$promise: deferred.promise};
         });
-
       $scope.updateUser($scope.user);
       $scope.$apply();
       expect($scope.errors).toEqual("error");
