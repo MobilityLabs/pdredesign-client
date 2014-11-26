f74d3cb01cb432413975637ea147d6c59859dfd7
Michael Davis <mkizziah38@gmail.com>
2014-11-26 09:58:08 -0500

    Specs added.

--- spec/javascripts/directives/faqsSpec.js
+++ spec/javascripts/directives/faqsSpec.js
@@ -4,7 +4,8 @@ describe('Directive: faqs', function() {
       $scope,
       $compile,
       $timeout,
-      $httpBackend;
+      $httpBackend,
+      $location;
 
   beforeEach(module('PDRClient'));
   beforeEach(inject(function($rootScope, $injector) {
@@ -12,7 +13,8 @@ describe('Directive: faqs', function() {
     $compile = $injector.get('$compile');
     $timeout = $injector.get('$timeout');
     $httpBackend = $injector.get('$httpBackend');
-
+    $location = $injector.get('$location');
+    $location.url("/faq");
     element = angular.element("<faqs></faqs>");
     $compile(element)($scope);
     $scope.$digest();
@@ -38,6 +40,109 @@ describe('Directive: faqs', function() {
     ];
     expect(isolatedScope.uniq_faq_property(categories, 'role'))
         .toEqual(['first', 'second', 'third']);
+  });
+
+  it('#preSelectedUrl is set by $location.url', function(){
+    expect(isolatedScope.preSelectedUrl)
+      .toEqual($location.url());
+  });
+
+  describe('#setSelectedRole', function(){
+    it('sets selectedRole to string param', function() {
+      expect(isolatedScope.selectedRole)
+        .not.toEqual('partner');
+      isolatedScope.setSelectedRole('partner');
+      expect(isolatedScope.selectedRole)
+        .toEqual('partner');
+    });
+  });
+
+  describe('#setSelectedTopic', function(){
+    it('sets selectedTopic to string param', function() {
+      expect(isolatedScope.selectedTopic)
+        .not.toEqual('general');
+      isolatedScope.setSelectedTopic('general');
+      expect(isolatedScope.selectedTopic)
+        .toEqual('general');
+    });
+  });
+
+  describe('#checkSelectedRole', function(){
+    it('returns partner if present in preSelectedUrl', function() {
+      isolatedScope.preSelectedUrl = "/faqs?partner";
+      isolatedScope.$digest();
+      expect(isolatedScope.checkSelectedRole()).toEqual('partner');
+    });
+
+    it('returns false at the proper time', function() {
+      isolatedScope.preSelectedUrl = "/faqs?paner";
+      isolatedScope.$digest();
+      expect(isolatedScope.checkSelectedRole()).toEqual(false);
+    });
+
+    describe('preSelectedUrl has partner', function(){
+      beforeEach(function(){
+        isolatedScope.preSelectedUrl = "/faqs?partner";
+        spyOn(isolatedScope, 'setSelectedRole');
+        spyOn(isolatedScope, 'setSelectedTopic');
+        isolatedScope.checkSelectedRole();
+      });
+
+      it('calls setSelectedRole', function() {
+        expect(isolatedScope.setSelectedRole)
+          .toHaveBeenCalled();
+      });
+
+      it('does not call checkSelectedTopic', function() {
+        expect(isolatedScope.setSelectedTopic)
+          .not.toHaveBeenCalled();
+      });
+
+    });
+
+  });
+
+  describe('checkSelectedTopic', function(){
+    beforeEach(function(){
+      isolatedScope.preSelectedUrl = "/faqs?partner?general";
+      spyOn(isolatedScope, 'setSelectedTopic');
+      isolatedScope.checkSelectedTopic();
+    });
+
+    it('calls checkSelectedTopic', function() {
+      expect(isolatedScope.setSelectedTopic)
+        .toHaveBeenCalled();
+    });
+  });
+
+  describe('#setOptionFromUrl', function(){
+    describe('#preSelectedUrl.length is greater then 7', function(){
+      beforeEach(function(){
+        isolatedScope.preSelectedUrl = "/faqs?hellohellohello";
+        spyOn(isolatedScope, 'checkSelectedRole')
+        spyOn(isolatedScope, 'checkSelectedTopic')
+      });
+
+      it('calls function checkSelectedRole', function() {
+        isolatedScope.setOptionFromUrl();
+        expect(isolatedScope.checkSelectedRole)
+          .toHaveBeenCalled();
+      });
+
+      it('calls function checkSelectedTopic', function() {
+        isolatedScope.setOptionFromUrl();
+        expect(isolatedScope.checkSelectedTopic)
+          .toHaveBeenCalled();
+      });
+    });
+
+    it('does not call function checkSelectedRole if preSelectedUrl.length is less then 7', function() {
+      spyOn(isolatedScope, 'checkSelectedRole')
+      isolatedScope.preSelectedUrl = "/faqs";
+      isolatedScope.setOptionFromUrl();
+      expect(isolatedScope.checkSelectedRole)
+        .not.toHaveBeenCalled();
+    });
   });
 
 });
